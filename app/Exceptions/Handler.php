7410faf1e6cf3bfedd2cf1324394ec26cc4d6d2e
Daniel Kahoun <113265820+danielkahoun@users.noreply.github.com>
2024-01-08 08:14:38 +0100

    log non-authenticated access & non-existent routes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +28,14 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, Request $request) {
+            Log::info($request->method() . ": " . $request->fullUrl() . " not found (client: " . $request->ip() . ")");
+            
+            return response()->json([
+                'message' => 'Record not found.'
+            ], 404);
         });
     }
 }
